8491e38162fb00954a08f6a5027cccf8a75c7f97
Eric Rincon <royeric77@gmail.com>
2023-07-13 07:54:59 -0500

    begun project

---
+++ pathfinder.py
@@ -0,0 +1,39 @@
+import pygame
+import math
+from queue import PriorityQueue 
+
+Width = 800
+WIN = pygame.display.set_mode((Width, Width))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+class Node:
+    def _init_(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_position(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+        
+
+
+
