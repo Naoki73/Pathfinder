97a0a57e5ff9a09815204b6fe9ebd829841cecf4
Eric Rincon <royeric77@gmail.com>
2023-07-17 05:36:11 -0500

    completed Node class

--- pathfinder.py
+++ pathfinder.py
@@ -40,6 +40,45 @@ class Node:
         #Tells us if we're in the open square
         return self.color == GREEN 
 
+    def is_barrier(self):
+        #Shows which squares are not enterable
+        return self.color == BLACK
 
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQUOISE
+    
+    def reset(self):
+        self.color == WHITE
 
+    def make_closed(self):
+        self.color = RED
 
+    def make_open(self):
+        #when called, make square the color its supposed to be
+        self.color = GREEN
+    
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, WIN):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+        
+
+def h(p1, p2):
+    #This is the heuristic 
+    (row, col)
